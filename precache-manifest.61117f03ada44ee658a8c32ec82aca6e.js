ea51f665b0756b80b608735046c4610cbfed89f0
alexsan-dev <as62971@gmail.com>
2020-05-11 19:38:58 -0600

    Updates

---
+++ precache-manifest.61117f03ada44ee658a8c32ec82aca6e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584bc5ed28a504145454ae26ac10f5bc",
+    "url": "/LightRoom-USAC/Manual/index.html"
+  },
+  {
+    "revision": "395fa4a2c48b20593a44",
+    "url": "/LightRoom-USAC/Manual/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "7abda98c4c1cb35137c7",
+    "url": "/LightRoom-USAC/Manual/static/css/main.22732168.chunk.css"
+  },
+  {
+    "revision": "395fa4a2c48b20593a44",
+    "url": "/LightRoom-USAC/Manual/static/js/2.be65ca1b.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/LightRoom-USAC/Manual/static/js/2.be65ca1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7abda98c4c1cb35137c7",
+    "url": "/LightRoom-USAC/Manual/static/js/main.e548cd00.chunk.js"
+  },
+  {
+    "revision": "871c0940a817ebf942be",
+    "url": "/LightRoom-USAC/Manual/static/js/runtime-main.1633b9e9.js"
+  },
+  {
+    "revision": "aa14349c7939242850f8b9d6cd720bb7",
+    "url": "/LightRoom-USAC/Manual/static/media/converter.aa14349c.jpg"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "/LightRoom-USAC/Manual/static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.d7e60f9d.woff2"
+  },
+  {
+    "revision": "99df2a28e209cd46f80dbf3f1ca88426",
+    "url": "/LightRoom-USAC/Manual/static/media/header-image.99df2a28.png"
+  },
+  {
+    "revision": "2cc076815b5e4bb90d592979dc94a7aa",
+    "url": "/LightRoom-USAC/Manual/static/media/header.2cc07681.jpg"
+  },
+  {
+    "revision": "2cadfe50bbaf2cf838a523fd7311fac3",
+    "url": "/LightRoom-USAC/Manual/static/media/ill.2cadfe50.gif"
+  },
+  {
+    "revision": "0b4eb4b853475d67922e15045b559840",
+    "url": "/LightRoom-USAC/Manual/static/media/login.0b4eb4b8.jpg"
+  },
+  {
+    "revision": "90eeb5adf29aee6ffa444f1442aa8c21",
+    "url": "/LightRoom-USAC/Manual/static/media/space.90eeb5ad.gif"
+  },
+  {
+    "revision": "f360eb06b42ce94fb5009f5bb225f5e0",
+    "url": "/LightRoom-USAC/Manual/static/media/viewer.f360eb06.jpg"
+  }
+]);
